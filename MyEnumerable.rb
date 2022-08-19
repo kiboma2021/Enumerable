cdff36d73682304f9a24975dd1298590abd08c90
Ben Kiboma <omayioben@gmail.com>
2022-08-19 20:57:48 +0300

    Add MyEnumerable module

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,3 +1,17 @@
 module MyEnumerable
-
-end
+  def any?
+    each {|i| return true if yield i}
+    false
+  end
+  
+  def all?
+    each {|i| return false unless yield i}
+    true
+  end
+  
+  def filter
+    filter_array =[]
+    each{|i| filter_array.push(i) if yield i}
+    filter_array
+  end
+end 
