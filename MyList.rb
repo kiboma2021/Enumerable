e4f7c7fb0a9fcf0aabc50064d017d12df0a0833d
darikmohammed <r.m.darik@gmail.com>
2022-08-19 21:03:30 +0300

    Add test for Enum

--- MyList.rb
+++ MyList.rb
@@ -10,3 +10,19 @@ class MyList
         list_length.times { |i| yield @list[i]}
     end
 end
+
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+list.all? {|e| e < 5}
+list.all? {|e| e > 5}
+
+# Test #any?
+
+list.any? {|e| e == 2}
+list.any? {|e| e == 5}
+
+# Test #filter
+
+list.filter {|e| e.even?}
